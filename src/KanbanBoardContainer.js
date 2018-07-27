db4e88235fa20be1bfe5585f0c50db7257f1660b
정승용(OFFICE-DEV) <jeongsy10@live.u1.ac.kr>
2018-07-27 19:50:31 +0900

    Fetch data from another network

---
+++ src/KanbanBoardContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import 'whatwg-fetch';
+import KanbanBoard from './KanbanBoard';
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+    'Content-Type': 'application/json',
+    Authentication: 'i-fetched-kanban-data'
+};
+
+class KanbanBoardContainer extends Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            cards: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(API_URL + '/cards', {headers: API_HEADERS})
+        .then((response) => response.json())
+        .then((responseData) => {
+            this.setState({cards: responseData});
+        })
+        .catch((error) => {
+            console.log('Error fetching and parsing data', error);
+        });
+    }
+
+    render() {
+        return <KanbanBoard cards={this.state.cards} />
+    }
+}
+
+export default KanbanBoardContainer;
